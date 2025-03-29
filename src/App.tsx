b9a9eb36fede3193cdc8d4a41de1512de16c09cb
DDanDev <110492441+DDanDev@users.noreply.github.com>
2025-03-29 20:48:05 -0300

    vite start

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import dayPlanLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+          <img src={dayPlanLogo} className="logo" alt="Vite logo" />
+      </div>
+      <h1>DayPlan - Organize seu tempo</h1>
+    </>
+  )
+}
+
+export default App
