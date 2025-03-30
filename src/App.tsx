35582810f7e724dd64e255e5f6e45080406b2e5b
DDanDev <110492441+DDanDev@users.noreply.github.com>
2025-03-29 22:41:57 -0300

    get back the landing page + basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import dayPlanLogo from '/vite.svg'
-import './App.css'
+import dayPlanLogo from '/DayPlan logo.png'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <>
-      <div>
-          <img src={dayPlanLogo} className="logo" alt="Vite logo" />
-      </div>
-      <h1>DayPlan - Organize seu tempo</h1>
-    </>
-  )
+const AppContainer = styled.div`
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+`
+
+const Logo = styled.img`
+    max-width: 90%;
+`
+
+export const App = () => {
+    return (
+        <AppContainer>
+            <Logo src={dayPlanLogo} alt='DayPlan - Organize seu tempo' />
+        </AppContainer>
+    )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import './index.scss'
+import {App} from './App.tsx'
+import styled from 'styled-components'
 
+const Footer = styled.footer`
+    background-color: #000000;
+    color: #fff;
+    text-align: center;
+    padding: 20px;
+    /* width: 100%; */
+    & p {
+        font-size: 0.5rem;
+    }
+`
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+        <Footer>
+            <p>© 2024 DayPlan. Todos os direitos reservados.</p>
+        </Footer>
+    </StrictMode>,
 )
